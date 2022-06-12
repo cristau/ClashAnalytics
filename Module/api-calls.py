49c636ed23c7c3709371e9bd22f9d62fe9fa0fae
cristau <cooperistau@gmail.com>
2022-06-11 22:47:01 -0500

    First table (get_all_member_info) is SQL ready

--- Module/api-calls.py
+++ Module/api-calls.py
@@ -1,63 +1,75 @@
 import requests
 import pandas as pd
 import json
-from api_headers import headers
+import numpy as np
+from Module.api_headers import headers
 
-#clan_tag = 9VG8P90Q
-#player_tag = QVGGVC02
+#MarketHood
+player_tag = 'QVGGVC02'
 
-# returns information regarding player info from the clan response (this only pulls player info that we cannot get from the player response)
-def get_player_info_from_clan(clan_tag):
-    response = requests.get(
-        'https://api.clashofclans.com/v1/clans/%23' + clan_tag, headers = headers)
-    json_response = response.json()
-    rows = []
-    for i in json_response['memberList']:
-        rows.append([i['tag'], i['clanRank'], i['previousClanRank']])
-    return(rows)
+#BabyHood
+player_tag = 'QJ8RPUCRL'
 
-df1 = pd.DataFrame(get_player_info_from_clan('9VG8P90Q'), columns = ["player_tag", "clan_rank", "previous_clan_rank"])
-print(df1)
+#Invidia Bandit
+clan_tag = '9VG8P90Q'
 
-# returns information regarding player info from the player response
+#Indian Prestige
+clan_tag = 'PQJUJCPL'
+
+# def get_member_list(clan_tag):
+#     response = requests.get(
+#         'https://api.clashofclans.com/v1/clans/%23' + clan_tag, headers = headers)
+#     json_response = response.json()
+#
+#     player_tags, player_names = [], []
+#     for i in json_response['memberList']:
+#         player_tags.append(i['tag'])
+#         player_names.append(i['name'])
+#
+#     clan_tag = json_response['tag']
+#     clan_name = json_response['name']
+#
+#     ML_dict = {'player_tag': player_tags, 'player_name': player_names, 'clan_tag': np.repeat(clan_tag, len(player_tags)), 'clan_name': np.repeat(clan_name, len(player_tags))}
+#     ML_df = pd.DataFrame(ML_dict)
+#     return(df)
+
+#my_dict = {0: {'player_name': 'MarketHood', 'player_tag': MarketHood},
+           # 1: {'player_name': 'BabyHood', 'player_tag': BabyHood}}
+
+
 def get_player_info(player_tag):
     response = requests.get(
-        'https://api.clashofclans.com/v1/players/%23' + player_tag, headers = headers)
+        'https://api.clashofclans.com/v1/players/%23' + player_tag, headers=headers)
     json_response = response.json()
-    rows = []
-    rows.append([json_response['tag'], json_response['name'], json_response['townHallLevel'], json_response['townHallWeaponLevel'],
-                 json_response['expLevel'], json_response['trophies'], json_response['bestTrophies'], json_response['warStars'],
-                 json_response['attackWins'], json_response['defenseWins'], json_response['builderHallLevel'], json_response['versusTrophies'],
-                 json_response['bestVersusTrophies'], json_response['versusBattleWins'], json_response['role'], json_response['warPreference'],
-                 json_response['donations'], json_response['donationsReceived'], json_response['clan']['tag'], json_response['clan']['name'],
-                 json_response['clan']['clanLevel'], json_response['league']['name']])
-    return(rows)
 
-df2 = pd.DataFrame(get_player_info('QVGGVC02'), columns = [
-    'player_tag', 'player_name', 'th_lvl', 'th_weapon_lvl', 'exp_lvl', 'trophies', 'best_trophies', 'war_stars',
-                                                           'attack_wins', 'defense_wins', 'bldr_hall_lvl', 'vs_trophies', 'best_vs_trophies', 'vs_battle_wins', 'role',
-                                                           'war_pref', 'donations', 'donations_rec', 'clan_tag', 'clan_name', 'clan_level', 'league_name'])
-print(df2)
+    breaking_list = ['clan', 'league', 'legendStatistics', 'achievements', 'versusBattleWinCount', 'labels', 'troops', 'heroes', 'spells']
+    missing_col = ['townHallWeaponLevel', 'role', 'warPreference']
 
-# returns a players troop information with their corresponding player tag
-def get_player_troop_info(player_tag):
+    player_dict = {}
+    for k, v in zip(json_response.keys(), json_response.values()):
+        if k in breaking_list:
+            break
+        player_dict.update({k: v})
+    if any(i in missing_col for i in list(player_dict.keys())) == False:
+        for i in missing_col:
+            player_dict.update({i: None})
+
+    player_df = pd.DataFrame(player_dict, index = [0])
+    return(player_dict)
+
+def get_all_member_info(clan_tag):
     response = requests.get(
-        'https://api.clashofclans.com/v1/players/%23' + player_tag, headers = headers)
+        'https://api.clashofclans.com/v1/clans/%23' + clan_tag, headers=headers)
     json_response = response.json()
-    rows = []
-    for i in json_response['troops']:
-        rows.append([json_response['tag'], i['name'], i['level'], i['maxLevel'], i['village']])
 
-    return(rows)
+    temp = []
+    for i in json_response['memberList']:
+        temp.append(i['tag'])
 
-df3 = pd.DataFrame(get_player_troop_info('QVGGVC02'), columns = [
-    'player_tag', 'troop_name', 'troop_lvl', 'max_lvl', 'village'
-])
-print(df3)
+    member_list = [i.replace('#', '') for i in temp]
 
-def get_all_clan_member_info(clan_tag):
-    '''
-    Grabs player information from the player response, but for every member in a given clan
-    :param clan_tag: a clan tag string
-    :return: A dataframe
-    '''
+    players = [get_player_info(i) for i in member_list]
+    all_member_data_df = pd.DataFrame(players)
+    return(all_member_data_df)
+
+print(get_all_member_info('9VG8P90Q'))
